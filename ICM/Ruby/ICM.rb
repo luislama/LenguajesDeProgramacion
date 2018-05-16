b99c390d358e3ad96201c2808d81f2065ccdd3e0
luislama <luislama@espol.edu.ec>
2018-05-16 18:20:57 -0500

    [UPD] ICM_js.html(rewrite method for eficiency), ICM.rb(using classes and the OOP)

--- ICM/Ruby/ICM.rb
+++ ICM/Ruby/ICM.rb
@@ -4,57 +4,92 @@ puts ''
 puts '     BMI    '
 puts ' ___________'
 puts ''
+
+
+
+
+class Person
+
+	def initialize(height, mass)
+		@p_height = height
+		@p_mass = mass
+	end
+	
+
+	def getHeight
+		@p_height
+	end
+	
+	def getMass
+		@p_mass
+	end
+
+end
+
+
+class BMIcalc
+
+	def self.calculateBMI (persona)
+		( (persona.getMass.to_f) / ( (persona.getHeight.to_f)*(persona.getHeight.to_f) ) )
+	end
+
+	def self.getCategoryByBMI (persona)
+
+		bmi = calculateBMI persona
+		categoryBMI = ''
+		
+		if bmi < 30
+			if bmi < 16
+				if bmi < 15
+					categoryBMI = "Very Severely Underweight"
+				else
+					categoryBMI = "Several Underweight"
+				end
+			elsif bmi >= 18.5
+				if bmi < 25
+					categoryBMI = "Healthy weight"
+				else
+					categoryBMI = "Overweight"
+				end
+			else
+				categoryBMI = "Underweight"
+			end
+		elsif bmi >= 35
+			if bmi < 45
+				if bmi < 40
+					categoryBMI = "Obesse Class II"
+				else
+					categoryBMI = "Obesse Class III"
+				end
+			elsif bmi >= 50
+				if bmi < 60
+					categoryBMI = "Obesse Class V"
+				else
+					categoryBMI = "Obesse Class VI"
+				end
+			else
+				categoryBMI = "Obesse Class IV"
+			end
+		else
+			categoryBMI = "Obesse Class I"
+		end
+		
+		bmiS = "%.2f" % bmi
+		puts "BMI: " + bmiS + " | Your category is " + categoryBMI +"!"	
+		
+	end
+
+end
+
+
+
+
 puts 'What is your mass in kg?'
 mass = gets.chomp.to_f
 puts ''
 puts 'What is your height in m?'
 height = gets.chomp.to_f
 puts ''
-bmi = (mass/(height*height))
 
-categoryBMI = ''
-if( bmi < 18.5 )
-	if( bmi >= 16 )
-		categoryBMI = "Underweight"
-	else
-		if( bmi >= 15 )
-			categoryBMI = "Several Underweight"
-		else
-			categoryBMI = "Very Severely Underweight"
-		end
-	end
-else
-	if( bmi >= 25 )
-		if( bmi >= 40 )
-			if( bmi < 45 )
-				categoryBMI = "Obesse Class III"
-			else
-				if( bmi < 50 )
-					categoryBMI = "Obesse Class IV"
-				else
-					if( bmi < 60 )
-						categoryBMI = "Obesse Class V"
-					else
-						categoryBMI = "Obesse Class VI"
-					end
-				end
-			end
-		else
-			if( bmi < 30 )
-				categoryBMI = "Overweight"
-			else
-				if( bmi < 35 )
-					categoryBMI = "Obesse Class I"
-				else
-					categoryBMI = "Obesse Class II"
-				end
-			end
-		end
-
-	else
-		categoryBMI = "Healthy weight"
-	end
-end
-
-bmiS = "%.2f" % bmi
-puts "BMI: " + bmiS + " | Your category is " + categoryBMI +"!"
+persona = Person.new(height,mass)
+BMIcalc.getCategoryByBMI persona
